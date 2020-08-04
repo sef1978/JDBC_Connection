7a3f12809b1b77ee1a84199078dd1f71172d52be
sefika arslan <sefikaarslan@sefikas-MacBook-Pro.local>
2020-08-04 14:32:55 -0700

    Java Database Connection

---
+++ src/JDBC/JDBC_Update.java
@@ -0,0 +1,67 @@
+package JDBC;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBC_Update {
+
+	public static void main(String[] args) throws SQLException {
+		
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+		
+		// The format for jdbc:mysql://hostname:port/databaseName
+		String url = "jdbc:mysql://localhost:3306/ebookshop" + "?serverTimezone=UTC";
+		String username = "root";
+		String password = "leyla";
+		
+		try {
+			connection = DriverManager.getConnection(url, username, password);
+			
+			statement = connection.createStatement();
+			String sqlUpdate = "update books set price = price * 0.9, qty = qty + 5 where id = 1";
+			System.out.println("The SQL statement is " + sqlUpdate);
+			
+			statement.executeUpdate(sqlUpdate);
+			System.out.println();
+			
+			// Increase the price by 50% "A cup of Java"
+			String sqlUpdate2 = "update books set price = price * 1.5 where id = 4";
+			System.out.println("The SQL statement is " + sqlUpdate2);
+			
+			statement.executeUpdate(sqlUpdate2);
+			resultSet = statement.executeQuery("select * from books");
+			
+			while (resultSet.next()) {
+				System.out.println(resultSet.getInt("id") + ", " + resultSet.getString("title") + ", "
+						+ resultSet.getString("author") + ", " + resultSet.getDouble("price") + ", "
+						+ resultSet.getInt("qty"));
+		}
+			
+			
+			// CRUD Create, Read, Update, Delete
+			  // Insert - Create
+			  // Read - Select
+			  // Update - Update
+			  // Delete- Delete
+			
+			
+		}catch (Exception e) {
+		//	System.out.println("Error connecting to the database");
+			e.printStackTrace();
+		}finally {
+			if(connection != null){
+				resultSet.close();
+			}
+			if(connection != null){
+				resultSet.close();
+			}
+			if(connection != null){
+				resultSet.close();
+			}
+		 }
+	}
+}
